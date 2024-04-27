a19d48d71b169e69a1a8ab49e403d5eed6704aa6
MabyDiaz <mabeldiaz7680@gmail.com>
2024-04-27 09:23:22 -0300

    modifico cajero.js

--- ejercitacion_clase2/cajero_automatico_ok/cajero.js
+++ ejercitacion_clase2/cajero_automatico_ok/cajero.js
@@ -1,18 +1,14 @@
-/* opcion = prompt(`\n**Cajero Automático**\n\n1. Consultar sal
- 
- do\n2. Depositar dinero\n3. Retirar dinero\n4. Salir\n\nElija una opción:`); */
-
-let nombreUsuario = prompt("Ingrese su nombre");
+let nombreUsuario = prompt('Ingrese su nombre');
 let saldoCuenta = 20000;
 let montoRetiro;
 let montoDeposito;
 
 function solicitarMontoRetiro() {
-  return parseFloat(prompt("Ingrese el monto a retirar"));
+  return parseFloat(prompt('Ingrese el monto a retirar'));
 }
 
 function solicitarMontoDeposito() {
-  return parseFloat(prompt("Ingrese el monto que desea depositar"));
+  return parseFloat(prompt('Ingrese el monto que desea depositar'));
 }
 
 function mostrarMensaje(mensaje) {
@@ -20,47 +16,47 @@ function mostrarMensaje(mensaje) {
 }
 
 function verificarMonto(monto) {
-  do {
-    if (monto === undefined) {
-      mostrarMensaje("Monto inválido. No se ha proporcionado un monto.");
-      return false;
-    } else if (isNaN(monto)) {
-      mostrarMensaje(
-        "Monto inválido. El valor ingresado no es un número válido."
-      );
-      return false;
-    } else if (monto <= 0) {
-      mostrarMensaje("Monto inválido. Por favor, ingrese un número positivo.");
-      return false;
-    } else if (monto > saldoCuenta) {
-      mostrarMensaje(
-        `${nombreUsuario}, tus fondos son insuficientes. Tu saldo disponible es: $${saldoCuenta} `
-      );
-      return false;
-    } else {
-      return true;
-    }
-  } while (monto === undefined || isNaN(monto) || monto <= 0);
+  if (monto === undefined) {
+    mostrarMensaje('Monto inválido. No se ha proporcionado un monto.');
+    return false;
+  } else if (isNaN(monto)) {
+    mostrarMensaje(
+      'Monto inválido. El valor ingresado no es un número válido.'
+    );
+    return false;
+  } else if (monto <= 0) {
+    mostrarMensaje('Monto inválido. Por favor, ingrese un número positivo.');
+    return false;
+  } else if (monto > saldoCuenta) {
+    mostrarMensaje(
+      `${nombreUsuario}, tus fondos son insuficientes. Tu saldo disponible es: $${saldoCuenta} `
+    );
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function retirarDinero() {
-  montoRetiro = solicitarMontoRetiro();
-  if (verificarMonto(montoRetiro)) {
-    saldoCuenta -= montoRetiro;
-    mostrarMensaje(
-      `${nombreUsuario}, tu retiro fue exitoso!! \nRetiraste: $${montoRetiro} \nTu nuevo saldo es: $${saldoCuenta}`
-    );
-  }
+  do {
+    montoRetiro = solicitarMontoRetiro();
+  } while (!verificarMonto(montoRetiro));
+
+  saldoCuenta -= montoRetiro;
+  mostrarMensaje(
+    `${nombreUsuario}, tu retiro fue exitoso!! \nRetiraste: $${montoRetiro} \nTu nuevo saldo es: $${saldoCuenta}`
+  );
 }
 
 function depositarDinero() {
-  montoDeposito = solicitarMontoDeposito();
-  if (verificarMonto(montoDeposito)) {
-    saldoCuenta += montoDeposito;
-    return mostrarMensaje(
-      `${nombreUsuario}, depositaste $${montoDeposito}. \nTu saldo actual es: $${saldoCuenta}`
-    );
-  }
+  do {
+    montoDeposito = solicitarMontoDeposito();
+  } while (!verificarMonto(montoDeposito));
+
+  saldoCuenta += montoDeposito;
+  return mostrarMensaje(
+    `${nombreUsuario}, tu depósito fue exitodo!!!\nDepositaste $${montoDeposito}. \nTu saldo actual es: $${saldoCuenta}`
+  );
 }
 
 function mostrarSaldo() {
@@ -72,16 +68,14 @@ function utilizarCajero() {
   do {
     opcion = parseInt(
       prompt(
-        "***** CAJERO AUTOMATICO *****\n\n1. Consultar Saldo\n2. Depositar Dinero\n3. Retirar Dinero\n4. Salir\n\nElija una opción"
+        '***** CAJERO AUTOMATICO *****\n\n1. Consultar Saldo\n2. Depositar Dinero\n3. Retirar Dinero\n4. Salir\n\nElija una opción'
       )
     );
 
     switch (opcion) {
       case 1:
         mostrarSaldo();
-        break; 
-         
-        
+        break;
       case 2:
         depositarDinero();
         break;
